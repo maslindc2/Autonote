485c0e24702341b8147d339a272171dc8e14609c
Maslin Farrell <maslinfar@gmail.com>
2019-12-18 15:34:29 -0800

    Started code to detect if file or dir exists

--- autonote.py
+++ autonote.py
@@ -16,49 +16,42 @@ extensions = {
     "java" : java
 }
 
-
-def create():
-    
+def checkFileAndFolder():
+    folderName = str(sys.argv[1])
+    fileName = str(sys.argv[2]) + str(sys.argv[3])
+    if(os.path.isdir(folderName)):
+        #os.mkdir(folderName)
+        os.chdir(folderName)
+        if not (os.path.isfile(fileName)):
+            createFile()
+   
+def createFile():
     extension = str(sys.argv[3])
-    folderName = str(sys.argv[2])
-    fileName = str(sys.argv[1])
-    os.chdir("./Notes")
+    fileName = str(sys.argv[2])
+    folderName = str(sys.argv[1])
 
     # Checks to see if the specified file type is in our dictionary of extensions
     # If it isn't then let the user know and default it to a txt file
     try:
         extension = extensions[extension]
     except Exception:
-        print(extension)
-        print("The given extensions is not in the programs dictionary")
-        print("I have instead created it as a text file")
-        extension = ".txt"
-    
-    #Gets the file name and appends the extension to it
-    fileName = fileName + extension
-    
-    if os.path.isdir("./" + folderName):
-        os.path.chdir("./" + folderName)
-    
-    else:
-        os.mkdir(folderName)
-        os.chdir("./" + folderName)
-    
-    if not os.path.isfile("./" + fileName):
-        open(fileName, "a").close()
-    
-    os.system("subl " + fileName)
+        print(extension + " is an unknown extension")
+        print("I have changed it to a txt file")
+        extension = ".txt"    
 
-    print(sys.argv)
-    #         Gets the file name  Gets the extension
-    fileName = str(sys.argv[1]) + str(sys.argv[3])
-    #Open fileName specified from above and open for writing and then close
+        #Prints out specified usage TODO delete this line in final version
+        print(sys.argv)
+
+    
+    fileName += extension
+
     open(fileName, "a").close()
+
     #Opens the file name in atom
-    os.system("atom " + fileName)
+    os.system("nano " + fileName)
     
 
 
 
 if __name__ == "__main__":
-    create()
+    checkFileAndFolder()
