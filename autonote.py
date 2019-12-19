6b958d29aa54a3a70bed473c0c38b718bc0aec27
Maslin Farrell <maslinfar@gmail.com>
2019-12-18 19:13:38 -0800

    Added comments

--- autonote.py
+++ autonote.py
@@ -25,9 +25,11 @@ def checkFileAndFolder():
     
     if not (os.path.isdir(folderName)):
         
-        #Create the directory and change to it using specified folder name
+        # Create the directory and change to it using specified folder name
         os.mkdir(folderName)
         os.chdir(folderName)
+
+        # Calls the createFile function
         createFile(folderName, fileName, extension)
         
     else:
@@ -37,15 +39,17 @@ def checkFileAndFolder():
 
 def openFile(fileName):
     
-    os.system("nano " + fileName)   
+    os.system("nano " + fileName)
 
 def lazyOpen(folderName):
     
+    # Function to open an already made folder and the last modifed file
     os.chdir(folderName)
     filelist = os.listdir(os.getcwd())
     filelist = filter(lambda x: not os.path.isdir(x), filelist)
     newest = max(filelist, key=lambda x: os.stat(x).st_mtime)
     
+    # Opens the newest file in nano
     os.system("nano " + newest)
     
 
@@ -61,12 +65,9 @@ def createFile(folderName, fileName, extension):
         print("I have changed it to a txt file")
         extension = ".txt"    
 
-        #Prints out specified usage TODO delete this line in final version
-        print(sys.argv)
-
+    # Creates a new filename
     open(fileName, "a").close()
-
-    #Opens the file name in atom
+    # Opens the file name in atom
     os.system("nano " + fileName)
     
 
@@ -74,8 +75,28 @@ def createFile(folderName, fileName, extension):
 
 if __name__ == "__main__":
     
+    #Decides which method to call based on the length of the given command
+    
+    # File creation command has a length of 4 so it calls checkFileAndFolder function
     if(len(sys.argv) == 4):
         checkFileAndFolder()
-    elif (len(sys.argv) == 2):
-        lazyOpen(sys.argv[1])
+
+    # If the length is 2 then the user's command should be ane folder_name
+    elif(len(sys.argv) == 2):
+        
+        # gets the folder name that the user specified
+        folderName = sys.argv[1]
+
+        #try to call lazyOpen if it fails due to the folder not existing if an error happens print the error message
+        try:
+            lazyOpen(folderName)
+        except OSError:
+
+            print("The folder has not been created yet please run the below command")
+            print("ane " + folderName +" name_of_file .extension_name ")
+    else:
+        print("Incorrect syntax")
+        print("To create a new note and folder use the below command")
+        print("ane name_of_folder name_of_file .extension_name")
+        
     
